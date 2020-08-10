528e492272fbd22fb4f20e5abf07ba1572287164
ShGoldfish <sdavari@vt.edu>
2020-08-10 18:24:09 -0400

    Need updating the app and face position to the same coordinate system

--- Assets/Scripts/AppManager.cs
+++ Assets/Scripts/AppManager.cs
@@ -6,9 +6,11 @@ public class AppManager : MonoBehaviour
 {
 	bool is_trans;
 	ContextDetection contextDetection;
+	private Camera cam;
 
 	private void Start()
 	{
+		cam = Camera.main;
 		contextDetection = GameObject.Find("Manager").GetComponent<ContextDetection>();
 		is_trans = false;
 	}
@@ -27,16 +29,29 @@ public class AppManager : MonoBehaviour
 
 	private bool Overlapping(List<int> faceBox)
 	{
-		int minX1, minY1, maxX1, maxY1, minX2, minY2, maxX2, maxY2;
+		float minX1, minY1, maxX1, maxY1, minX2, minY2, maxX2, maxY2;
 		minX1 = Convert.ToInt32(gameObject.GetComponent<SpriteRenderer>().sprite.bounds.min.x);
 		minY1 = Convert.ToInt32(gameObject.GetComponent<SpriteRenderer>().sprite.bounds.min.y);
 		maxX1 = Convert.ToInt32(gameObject.GetComponent<SpriteRenderer>().sprite.bounds.max.x);
-		maxY1 = Convert.ToInt32(gameObject.GetComponent<SpriteRenderer>().sprite.bounds.max.x);
+		maxY1 = Convert.ToInt32(gameObject.GetComponent<SpriteRenderer>().sprite.bounds.max.y);
+		print("App: " + minX1 + ", " + minY1 + ", " + maxX1 + ", " + maxY1);
+
+		//Vector3 point1 = new Vector3();
+		//Vector3 point2 = new Vector3();
+		//point1 = cam.ScreenToWorldPoint(new Vector3(minX1, minY1, cam.nearClipPlane));
+		//point2 = cam.ScreenToWorldPoint(new Vector3(maxX1, maxY1, cam.nearClipPlane));
+		//print("App: " + point1.x + ", " + point1.y + ", " + point2.x + ", " + point2.y);
+
 
 		minX2 = faceBox[0];
 		minY2 = faceBox[1];
 		maxX2 = faceBox[2];
 		maxY2 = faceBox[3];
+		print("Face: " + minX2 + ", " + minY2 + ", " + maxX2 + ", " + maxY2);
+
+		//point1 = cam.ScreenToWorldPoint(new Vector3(minX2, minY2, cam.nearClipPlane));
+		//point2 = cam.ScreenToWorldPoint(new Vector3(maxX2, maxY2, cam.nearClipPlane));
+		//print("Face 2: " + point1.x + ", " + point1.y + ", " + point2.x + ", " + point2.y);
 
 		if ((maxX1 <= maxX2 && maxX1 >= minX2) || (minX1 <= maxX2 && minX1 >= minX2))
 		{
