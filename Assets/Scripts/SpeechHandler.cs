b6600b565de8f50415d4c562572caa6850e37525
ShGoldfish <sdavari@vt.edu>
2020-12-09 18:22:57 -0500

    speach handler code cleanup

--- Assets/Scripts/SpeechHandler.cs
+++ Assets/Scripts/SpeechHandler.cs
@@ -16,7 +16,6 @@ public class SpeechHandler : MonoBehaviour, IMixedRealitySpeechHandler
     private static string deviceName = string.Empty;
     private int samplingRate;
     private const int messageLength = 15;
-    //private StringBuilder textSoFar;
 	
     private void Awake()
     {       
@@ -26,37 +25,22 @@ public class SpeechHandler : MonoBehaviour, IMixedRealitySpeechHandler
         // This event is fired while the user is talking. As the recognizer listens, it provides text of what it's heard so far.
         dictationRecognizer.DictationHypothesis += DictationRecognizer_DictationHypothesis;
 
-        // 3.a: Register for dictationRecognizer.DictationResult and implement DictationResult below
-        // This event is fired after the user pauses, typically at the end of a sentence. The full recognized string is returned here.
-        //dictationRecognizer.DictationResult += DictationRecognizer_DictationResult;
-
         // 3.a: Register for dictationRecognizer.DictationComplete and implement DictationComplete below
         // This event is fired when the recognizer stops, whether from Stop() being called, a timeout occurring, or some other error.
         dictationRecognizer.DictationComplete += DictationRecognizer_DictationComplete;
         
-        // For Future maybe
-        //currentlySaying = new StringBuilder();
-        //textSoFar = new StringBuilder("");
-
         PhraseRecognitionSystem.Shutdown();
         dictationRecognizer.Start();
         // Query the maximum frequency of the default microphone. Use 'unused' to ignore the minimum frequency.
         int unused;
         Microphone.GetDeviceCaps(deviceName, out unused, out samplingRate);
-
-        // Query the maximum frequency of the default microphone. Use 'unused' to ignore the minimum frequency.
-        //int unused;
-        //Microphone.GetDeviceCaps(deviceName, out unused, out samplingRate);
-        //Microphone.Start(deviceName, false, messageLength, samplingRate);
+		Microphone.Start(deviceName, false, messageLength, samplingRate);
     }
 	private void Update()
 	{
-		//if (textSoFar != null && textSoFar.Length != 0)
-		//	Manager.Set_isTalking(true);
 		if (!Manager.Get_isTalking())
 		{
 			dictationRecognizer.Start();
-			//Microphone.Start(deviceName, false, messageLength, samplingRate);
 		}
 	}
 
@@ -70,8 +54,7 @@ public class SpeechHandler : MonoBehaviour, IMixedRealitySpeechHandler
 		{
 			Manager.Set_isTalking(true);
 			RecognizeMyKeywords(text.ToLower());
-		}//currentlySaying.Append(text);
-		//textSoFar.Append(text);
+		}
     }
 
 	private void RecognizeMyKeywords(string text)
@@ -97,19 +80,6 @@ public class SpeechHandler : MonoBehaviour, IMixedRealitySpeechHandler
 		Manager.Set_SpeechContext(context);
 	}
 
-	/// <summary>
-	/// This event is fired after the user pauses, typically at the end of a sentence. The full recognized string is returned here.
-	/// </summary>
-	/// <param name="text">The text that was heard by the recognizer.</param>
-	/// <param name="confidence">A representation of how confident (rejected, low, medium, high) the recognizer is of this recognition.</param>
-	//private void DictationRecognizer_DictationResult(string text, ConfidenceLevel confidence)
- //   {
- //       // 3.a: Append textSoFar with latest text
- //       if (text != null && text.Length != 0)
-	//		Manager.Set_isTalking(true);
-	//	textSoFar.Append(text + ". ");
- //   }
-
     /// <summary>
     /// This event is fired when the recognizer stops, whether from Stop() being called, a timeout occurring, or some other error.
     /// Typically, this will simply return "Complete". In this case, we check to see if the recognizer timed out.
@@ -117,38 +87,11 @@ public class SpeechHandler : MonoBehaviour, IMixedRealitySpeechHandler
     /// <param name="cause">An enumerated reason for the session completing.</param>
     private void DictationRecognizer_DictationComplete(DictationCompletionCause cause)
     {
-        // If Timeout occurs, the user has been silent for too long.
-        // With dictation, the default timeout after a recognition is 20 seconds.
-        // The default timeout with initial silence is 5 seconds.
         if (cause == DictationCompletionCause.TimeoutExceeded)
         {
-            //Microphone.End(deviceName);
 			Manager.Set_isTalking(false);
-			//currentlySaying = new StringBuilder();
-			//textSoFar = new StringBuilder("");
         }
     }
-
-    //public void StopRecording()
-    //{
-    //    // 3.a: Check if dictationRecognizer.Status is Running and stop it if so
-    //    if (dictationRecognizer.Status == SpeechSystemStatus.Running)
-    //    {
-    //        dictationRecognizer.Stop();
-    //    }
-    //    Microphone.End(deviceName);
-    //}
-    //public AudioClip StartRecording()
-    //{
-    //    // 3.a Shutdown the PhraseRecognitionSystem. This controls the KeywordRecognizers
-    //    PhraseRecognitionSystem.Shutdown();
-
-    //    // 3.a: Start dictationRecognizer
-    //    dictationRecognizer.Start();
-
-    //    // Start recording from the microphone for 10 seconds.
-    //    return Microphone.Start(deviceName, false, messageLength, samplingRate);
-    //}
 
     /// <summary>
     /// The dictation recognizer may not turn off immediately, so this call blocks on
