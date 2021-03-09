c9c577b373df103499e93c1f7b737648a6df24fe
ShGoldfish <sdavari@vt.edu>
2021-03-08 22:42:42 -0500

    add FileLog

---
+++ Assets/Scripts/FileLog.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine.UI;
+using UnityEngine;
+#if WINDOWS_UWP
+using Windows.Storage;
+#endif
+
+public class FileLog : MonoBehaviour
+{
+    public string fileName = "log.csv";
+    string path;
+
+    // Use this for initialization
+    public void SetHeader(string header) // header is like "time, accuracy, level, ......"
+    {
+#if WINDOWS_UWP
+        // Get local folder on HoloLense
+        path = Windows.Storage.ApplicationData.Current.LocalFolder.Path;
+#else
+        path = ".\\";
+#endif
+        File.AppendAllText(path + "\\" + fileName, "\n" + header + "\n");
+    }
+
+    public void WriteLine(string line) // you pass in one trial's date "1.1, 1, 4, ..."
+    {
+        File.AppendAllText(path + "\\" + fileName, "\n" + line);
+    }
+}
